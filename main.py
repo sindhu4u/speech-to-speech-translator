d2bc384d97f4f5ce43f5ed3eac3ae64a5cd98f72
Sindhu <122135856+sindhu4u@users.noreply.github.com>
2025-05-15 20:38:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from app import app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render sets this
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0",port=port)
